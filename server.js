165e33e085aa6c588fecd78b6ea257d372885840
Paul Vegan <buurzuum@gmail.com>
2017-07-29 14:22:26 +0200

    jakies guwno jakies

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ var hbs = exphbs.create({
 });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-// app.set('port', process.env.PORT || 3000);
 app.use(compression());
 app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -117,10 +116,7 @@ if (app.get('env') === 'production') {
   });
 }
 
-// app.listen(app.get('port'), function() {
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
